0ad71433ffe09848a115b22b956fbd516cf0ad0e
Akshay Jagadeesh <akjags@gmail.com>
2018-04-21 22:25:57 -0700

    updating preprocessing script

--- preprocess_image.py
+++ preprocess_image.py
@@ -1,9 +1,10 @@
 import scipy.misc
+from imageio import imread
 import numpy as np
 
 def preprocess_im(path):
     MEAN_VALUES = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
-    image = scipy.misc.imread(path)
+    image = imread(path)
     # Resize the image for convnet input, there is no change but just
     # add an extra dimension.
     image = np.reshape(image, ((1,) + image.shape))
@@ -18,3 +19,13 @@ def postprocess_im(img):
     image = np.clip(image, 0, 255).astype('uint8')
     return image
 
+if __name__ == "__main__":
+    # First, preprocess the images.
+    orig_ims = os.listdir('orig_ims/')
+
+    for im in orig_ims:
+        if '.jpg' in im:
+            imName = im.split('.')[0]
+            new_im = preprocess_im('orig_ims/'+im)
+            np.save('orig_ims/'+imName, new_im)
+            print 'Preprocessed image ' + imName + ' saved' 
