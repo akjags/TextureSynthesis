a074752482c9bde22702aaf58bee5a71dfd996ab
Akshay <akjags@gmail.com>
2018-05-30 17:50:19 -0700

    Updating pre and post processing scripts

--- preprocess_image.py
+++ preprocess_image.py
@@ -1,5 +1,5 @@
-import scipy.misc
-from imageio import imread
+from skimage.io import imread
+import os
 import numpy as np
 
 def preprocess_im(path):
@@ -10,13 +10,6 @@ def preprocess_im(path):
     image = np.reshape(image, ((1,) + image.shape))
     # Input to the VGG model expects the mean to be subtracted.
     image = image - MEAN_VALUES
-    return image
-
-def postprocess_im(img):
-    MEAN_VALUES = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
-    image = img + MEAN_VALUES
-    image = image[0]
-    image = np.clip(image, 0, 255).astype('uint8')
     return image
 
 if __name__ == "__main__":
