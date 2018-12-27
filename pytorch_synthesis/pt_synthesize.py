8f5e9d6b81fd46cba08e7f2cafc727740a62b96e
Akshay Jagadeesh <akjags@gmail.com>
2018-12-26 17:00:05 -0800

    Updating pt synthesis script

--- pytorch_synthesis/pt_synthesize.py
+++ pytorch_synthesis/pt_synthesize.py
@@ -14,6 +14,7 @@ import torchvision.models as models
 import copy
 from pt_tex_synth import *
 import argparse
+import pdb
 
 # desired size of the output image
 imsize = 256 if torch.cuda.is_available() else 128  # use small size if no gpu
@@ -47,14 +48,16 @@ def imsave(tensor, savepath=None):
 
 ########## MAIN: SPECIFY OPTIONS:
 if __name__ == "__main__":
-  ## Parse arguments
+  ### Parse arguments
   parser = argparse.ArgumentParser()
-  parser.add_argument("-p", "--path", help="specify the path of the original image")
-  parser.add_argument("-o", "--outdir", help="specify the path of the output directory")
-  parser.add_argument("-l", "--layer", help="specify the layer to match statistics through")
-  parser.add_argument("-n", "--nSplits", help="specify the number of splits")
+  parser.add_argument("-i", "--img_path", default="/home/users/akshayj/TextureSynthesis/stimuli/textures/orig_color/cherries.jpg", help="specifies the path of the original image")
+  parser.add_argument("-o", "--out_dir", default="/home/users/akshayj/TextureSynthesis/stimuli/textures/out_color/v1", help="specifies the path of the output directory")
+  parser.add_argument("-l", "--layer", default="pool2", help="specifies the layer to match statistics through")
+  parser.add_argument("-s", "--nSplits", default=2, help="specifies the number of sections to split each dimension into (e.g. 1x1, 2x2, nxn)")
+  parser.add_argument('-n', '--nSteps', default=5000, help="specifies the number of steps to run the gradient descent for")
   args = parser.parse_args()
 
+  ### PyTorch Models (VGG)
   # Load the model (VGG19)
   cnn = models.vgg19(pretrained=True).features.to(device).eval()
 
@@ -62,20 +65,26 @@ if __name__ == "__main__":
   cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
   cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
-  # Specify num_steps
-  num_steps = 5000
+  ### Specify texture synthesis parameters.
+  # Load the original image
+  style_img = image_loader(args.img_path)
+  img_name = args.img_path.split('/')[-1].split('.')[0] # get just the name (e.g. cherries) without path or extension
 
-  # Specify the style image to match
-  style_img = image_loader("/home/users/akshayj/TextureSynthesis/stimuli/textures/orig_color/cherries.jpg")
+  # Specify which layers to match
+  all_layers = ['conv_1_1', 'pool_1', 'pool_2', 'pool_4'];
+  style_layers = {'conv1_1': all_layers[:1], 'pool1': all_layers[:2], 'pool2': all_layers[:3], 'pool4':all_layers}
+  this_layers = style_layers[args.layer]
 
-  # Specify which layers
-  style_layers = ['conv_1_1', 'pool_1', 'pool_2', 'pool_4'];
-
+  ## Run Texture Synthesis
   # Randomly initialize white noise input image
   input_img = torch.randn(style_img.data.size(), device=device)
 
+  # Check if we're on GPU or CPU, then run! 
+  print("Using GPU" if torch.cuda.is_available() else "Using CPU")
   output_leaves = run_texture_synthesis(cnn, cnn_normalization_mean, cnn_normalization_std,
-                              style_img, input_img, num_steps=num_steps, style_layers=style_layers)
+                              style_img, input_img, num_steps=args.nSteps, style_layers=style_layers)
 
-  imsave(output_leaves, 'out_cherries2.png');
+  # Save final product to output directory
+  saveName = args.nSplits + 'x' + args.nSplits + '_' + args.layer + '_' + img_name + '.png'
+  imsave(output_leaves, args.out_dir + '/' + saveName);
 
