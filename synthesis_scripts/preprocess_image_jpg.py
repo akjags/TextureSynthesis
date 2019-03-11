8405360a6ee1e00381c4691550ea244c65a5698a
Akshay <akjags@gmail.com>
2019-03-10 23:49:14 -0700

    adding preprocessing files

---
+++ synthesis_scripts/preprocess_image_jpg.py
@@ -0,0 +1,24 @@
+from skimage.io import imread
+import os
+import numpy as np
+import sys
+
+def preprocess_im(path):
+    MEAN_VALUES = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+    image = imread(path)
+    # Resize the image for convnet input, there is no change but just
+    # add an extra dimension.
+    image = np.reshape(image, ((1,) + image.shape))
+    # Input to the VGG model expects the mean to be subtracted.
+    image = image - MEAN_VALUES
+    return image
+
+if __name__ == "__main__":
+    # First, preprocess the images.
+    args = sys.argv
+    orig_im_dir = args[1]
+    img_name = args[2]
+
+    new_im = preprocess_im(orig_im_dir+'/'+img_name+'.jpg')
+    np.save(orig_im_dir+'/'+img_name, new_im)
+    print 'Preprocessed image ' + img_name + ' saved'
